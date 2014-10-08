9cb86777998e8ccb45d9e11dd249ae7047ee8403
M. Maxwell Watson <hi@mmwtsn.com>
2014-10-08 13:32:03 -0400

    adds compass

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
+
+preferred_syntax = :scss
+output_style = :compressed
+line_comments = false
