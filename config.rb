8d0857bfc121a272276eba11d4317908b46e2d1e
M. Maxwell Watson <hi@mmwtsn.com>
2014-10-08 13:56:42 -0400

    images/ -> img/

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'compass/import-once/activate'
 http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
-images_dir = "images"
+images_dir = "img"
 javascripts_dir = "js"
 
 preferred_syntax = :scss
