8bc6f9b2934d034b6e04d37412d7d4f3dddaee49
M. Maxwell Watson <hi@mmwtsn.com>
2014-10-10 02:19:06 -0400

    nested mobile nav toggle

--- js/nav.js
+++ js/nav.js
@@ -11,4 +11,18 @@ $(document).ready(function() {
     $('.mobile-link').removeClass('active');
     $(this).addClass('active');
   });
+
+  $('.nested-mobile-link').on('click', function(e) {
+    e.preventDefault();
+
+    var $this = $(this);
+
+    if($this.hasClass('active')) {
+      $this.toggleClass('active');
+    }
+    else {
+      $('.nested-mobile-link').removeClass('active');
+      $this.addClass('active');
+    }
+  });
 });
