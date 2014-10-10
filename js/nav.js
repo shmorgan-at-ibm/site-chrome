5d14608f0dd62a4a618fc8de3bc8bc610112c4cb
M. Maxwell Watson <hi@mmwtsn.com>
2014-10-10 00:38:51 -0400

    mobile toggle, pass active

--- js/nav.js
+++ js/nav.js
@@ -1,2 +1,14 @@
 $(document).ready(function() {
+  $('.mobile-nav-toggle').on('click', function(e) {
+    e.preventDefault();
+
+    $('body').toggleClass('mobile-nav-open');
+  });
+
+  $('.mobile-link').on('click', function(e) {
+    e.preventDefault();
+
+    $('.mobile-link').removeClass('active');
+    $(this).addClass('active');
+  });
 });
