80fb5a38353b830103cdd2655a453fdab3dc2493
M. Maxwell Watson <hi@mmwtsn.com>
2014-10-15 16:39:28 -0400

    removes dist/ contents that are now git ignored

--- dist/footer.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $('#ibm-cookie-preferences-link, #ibm-close-cookie-overlay').on('click', function() {
-    $('#ibm-cloud-cookie-overlay').toggleClass('open');
-  });
-});
